7d7fd81f5fce9da4d47dee39b02ebe206ae0d04f
Stas Makar <stas.makar@gmail.com>
2014-09-23 18:12:58 +0300

    Added all files

---
+++ vim_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function link_file {
+    source="${PWD}/$1"
+    target="${HOME}/$1"
+
+    if [ -f "${target}" ]; then
+        mv ${target}{,.$(date +%F).bak}
+    fi
+
+    ln -sf ${source} ${target}
+}
+
+if [ -d ~/.dotfiles ]; then
+    pushd ~/.dotfiles/
+    git pull origin master
+    popd
+    vim -u ~/.dotfiles/.vimrc +BundleInstall! +BundleClean! +qall
+else
+    git clone https://github.com/stamak/dotfiles.git ~/.dotfiles
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+    pushd ~/.dotfiles
+    link_file .vimrc 
+    popd
+    
+    vim -u ~/.dotfiles/.vimrc +BundleInstall +qall
+fi
